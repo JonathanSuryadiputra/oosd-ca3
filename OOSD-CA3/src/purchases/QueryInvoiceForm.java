fb6d7153c6085aaf7ee6f53a8477deed0a13434b
Jonathan <Jonathan@149.153.131.87>
2020-02-06 20:27:29 +0000

    Merge add JFrame classes to the main query frame

--- OOSD-CA3/src/purchases/QueryInvoiceForm.java
+++ OOSD-CA3/src/purchases/QueryInvoiceForm.java
@@ -226,6 +226,203 @@ public class QueryInvoiceForm {
    
    /* -----------------------------------------------------------add Button----------------------------------------------------------*/
    private class addButtonHandler implements ActionListener {
+	   
+	   //JFrame class
+	   public class AddInvoiceForm extends JFrame {
+			
+			static final String DATABASE_URL = "jdbc:mysql://localhost/purchases";
+			static final String UserName_SQL = "root";
+			static final String Password_SQL = "password";
+			
+			JComboBox customerList;
+			JComboBox productList;
+			JTextField qtyProductField = new JTextField();
+			JButton submitButton = new JButton("Add Record");
+			JButton clearButton = new JButton("Clear Form");
+			JPanel form = new JPanel();
+			JPanel button = new JPanel();
+			Border padding1;
+			Border padding2;
+			
+			//get customer list for Customer JComboBox
+			public JComboBox getCustomerList() {
+				JComboBox customerSelectBox = new JComboBox();
+				Connection connection = null;
+				Statement statement = null;
+				
+				try {
+					connection = DriverManager.getConnection(DATABASE_URL, UserName_SQL, Password_SQL);
+					statement = connection.createStatement();
+					ResultSet customerResultSet = statement.executeQuery("select customerId, firstName, lastName from customer;");
+					ResultSetMetaData customerMetaData = customerResultSet.getMetaData();
+					int numberOfColumns = customerMetaData.getColumnCount();
+					while(customerResultSet.next() ) {
+						for ( int i = 1; i <= numberOfColumns; i=i+3 ) {
+							customerSelectBox.addItem("ID (" + customerResultSet.getObject(i) + ")   " + customerResultSet.getObject(i+1) + " " + customerResultSet.getObject(i+2));
+						}
+					}//end while
+				}//end try
+					
+				catch(SQLException sqlException ) {
+					sqlException . printStackTrace ();
+				}//end catch
+					
+				finally {
+					try{
+						statement. close ();	
+						connection. close ();
+					}//end try
+						
+					catch ( Exception exception ){	
+						exception . printStackTrace ();
+					}//end catch
+				}//end finally
+				
+				return customerSelectBox;
+			}
+			
+			//get Product list for Product JComboBox
+			public JComboBox getProductList() {
+				JComboBox productSelectBox = new JComboBox();
+				
+				Connection connection = null;
+				Statement statement = null;
+				   
+				   
+				try {
+					connection = DriverManager.getConnection(DATABASE_URL, UserName_SQL, Password_SQL);	
+					statement = connection.createStatement();	
+					ResultSet productResultSet = statement.executeQuery("select productId, productName, price from product;");	
+					ResultSetMetaData productMetaData = productResultSet.getMetaData();	
+					int numberOfColumns = productMetaData.getColumnCount();
+					while(productResultSet.next() ) {	
+						for ( int i = 1; i <= numberOfColumns; i=i+3 )	
+							productSelectBox.addItem("ID (" + productResultSet.getObject(i) + ")   " + productResultSet.getObject(i+1) + " - \u20ac" + productResultSet.getObject(i+2));
+					}//end while
+				}//end try
+						
+					
+				catch(SQLException sqlException ) {	
+					sqlException . printStackTrace ();
+				}//end catch
+						
+					
+				finally {
+					try{		
+						statement. close ();
+						connection. close ();
+					}//end try
+			
+					catch ( Exception exception ){	
+						exception . printStackTrace ();
+					}//end catch
+				}//end finally
+				
+				return productSelectBox;
+			}
+			
+			//Constructor
+			public AddInvoiceForm(String title) {
+				super(title);
+				
+				getContentPane().setLayout(new BorderLayout());
+				
+				form.setLayout(new GridLayout(6, 1));
+				padding1 = BorderFactory.createEmptyBorder(10, 30, 10, 30); //padding for main form panel
+				form.setBorder(padding1);
+				
+				form.add(new JLabel("Customer ID"));
+				customerList = getCustomerList();
+				form.add(customerList); 
+				form.add(new JLabel("Product ID"));
+				productList = getProductList();
+				form.add(productList); 
+				form.add(new JLabel("Quantity"));  
+				form.add(qtyProductField);
+				
+				button.setLayout(new GridLayout(1, 2));
+				padding2 = BorderFactory.createEmptyBorder(0, 10, 20, 10); //padding for button panel
+				button.setBorder(padding2);
+				
+				SubmitButtonHandler submitHandler = new SubmitButtonHandler();
+				submitButton.addActionListener(submitHandler);
+				button.add(submitButton);
+				
+				ClearButtonHandler clearHandler = new ClearButtonHandler();
+				clearButton.addActionListener(clearHandler);
+				button.add(clearButton);
+				
+				add(form, BorderLayout.CENTER);
+				add(button, BorderLayout.SOUTH);
+			}
+			
+			//TextField handler
+			public class SubmitButtonHandler implements ActionListener {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					//get customer value
+					String customerIdstr = customerList.getSelectedItem().toString();
+					customerIdstr = customerIdstr.substring(customerIdstr.indexOf("(")+1,customerIdstr.indexOf(")"));
+					int customerId = Integer.parseInt(customerIdstr);
+					
+					//get product value
+					String productIdstr = productList.getSelectedItem().toString();
+					productIdstr = productIdstr.substring(productIdstr.indexOf("(")+1,productIdstr.indexOf(")"));
+					int productId = Integer.parseInt(productIdstr);
+					
+					//get quantity value
+					int qtyProduct = Integer.parseInt(qtyProductField.getText());
+					
+					if (qtyProduct < 1 || qtyProduct > 99) {
+						JOptionPane.showMessageDialog(null,"Invalid Quantity input");
+					}
+				 	
+					else {
+						
+						SimpleDateFormat formatDate = new SimpleDateFormat("yyyy-MM-dd");
+						SimpleDateFormat formatTime = new SimpleDateFormat("HH:mm:ss");
+						Date d = new Date();
+						
+						Connection connection = null;
+						Statement statement = null;
+							    
+						try {
+							connection = DriverManager.getConnection(DATABASE_URL, UserName_SQL, Password_SQL);
+							statement = connection.createStatement();
+							statement.executeUpdate("INSERT INTO invoice (customerId, productId, qtyProduct, invoiceDate, invoiceTime)" + " VALUES " + "(" + customerId + "," + productId + "," + qtyProduct + ",'" + formatDate.format(d) + "','" + formatTime.format(d) + "')");
+						}
+						
+						catch(SQLException sqlException) {
+							sqlException.printStackTrace();
+						}
+									
+								
+						finally {
+							try {		
+								statement.close();		
+								connection.close();
+							}
+							
+							catch ( Exception exception ) {
+								exception.printStackTrace();
+							}
+						}
+						
+						JOptionPane.showMessageDialog(null,"Created New Record");
+					}//end else
+				}
+			}
+			
+			//Clear button handler
+			public class ClearButtonHandler implements ActionListener {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					customerList.setSelectedIndex(-1);
+					productList.setSelectedIndex(-1);
+					qtyProductField.setText("");
+				}
+			}
+		}// end JFrame class
 
 	   	@Override
 		public void actionPerformed(ActionEvent e) {
--- OOSD-CA3/src/purchases/QueryProductForm.java
+++ OOSD-CA3/src/purchases/QueryProductForm.java
@@ -5,6 +5,7 @@ import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
@@ -235,6 +236,143 @@ public class QueryProductForm {
    
    /* -----------------------------------------------------------add Button----------------------------------------------------------*/
    private class addButtonHandler implements ActionListener {
+	   
+	   //JFrame class
+	   public class AddProductForm extends JFrame {
+			private JTextField productNameField = new JTextField();
+			private JTextField descriptionField = new JTextField();
+			private JTextField priceField = new JTextField();
+			private JButton submitButton = new JButton("Add Product");
+			private JButton clearButton = new JButton("Clear Form");
+			JPanel form = new JPanel();
+			JPanel buttons = new JPanel();
+			Border padding1;
+			Border padding2;
+
+
+				// Constructor 
+				public AddProductForm(String title) {
+					// Set title for frame and choose layout
+					super(title);
+					getContentPane().setLayout(new BorderLayout());
+					form.setLayout(new GridLayout(8, 2));
+					
+					padding1 = BorderFactory.createEmptyBorder(10, 30, -40, 30);
+					form.setBorder(padding1);
+					
+					// Add Product Name label and text field to frame
+					form.add(new JLabel("Product Name"));
+					form.add(productNameField);
+
+					// Add Description label and text field to frame
+					form.add(new JLabel("Decription"));
+					form.add(descriptionField);
+
+					// Add Price label and text field to frame
+					form.add(new JLabel("Price " + "\u20ac"));
+					form.add(priceField);
+
+					padding2 = BorderFactory.createEmptyBorder(0, 30, 20, 30);
+					buttons.setBorder(padding2);
+					buttons.setLayout(new GridLayout(1, 2));
+					// Add submit button to frame
+					buttons.add(submitButton);
+
+					SubmitButtonHandler submitHandler = new SubmitButtonHandler();
+					submitButton.addActionListener(submitHandler);
+
+					//Add clear button to frame
+					buttons.add(clearButton);
+					
+					ClearButtonHandler clearHandler = new ClearButtonHandler();
+					clearButton.addActionListener(clearHandler);
+					
+					add(form, BorderLayout.CENTER);
+					add(buttons, BorderLayout.SOUTH);
+				}// end constructor
+
+				// TextField handler
+				private class SubmitButtonHandler implements ActionListener {
+					
+					String productName;
+					String description;
+					String price;
+					
+					final String DATABASE_URL = "jdbc:mysql://localhost/purchases";
+					final String UserName_SQL = "root";
+					final String Password_SQL = "password";
+					
+					Connection connection = null;
+					Statement statement = null;
+
+					@Override
+					public void actionPerformed(ActionEvent event) {
+						productName = productNameField.getText();
+						description = descriptionField.getText();
+						price = priceField.getText();
+						
+						if (productName.isEmpty() || description.isEmpty() || price.isEmpty()) {
+							JOptionPane.showMessageDialog(AddProductForm.this, String.format("One or more empty field(s), will not add to database", event.getActionCommand()));
+						}
+						else if (productName.matches("^[a-zA-Z-'.\\s]+") == false || description.matches("^[a-zA-Z-' .\\s]+") == false || price.matches("^[+0-9,\\s]+") == false) {
+							JOptionPane.showMessageDialog(AddProductForm.this, String.format("One or more invalid input(s), please try again", event.getActionCommand()));
+						}
+						else {
+						
+						try {
+							
+							// establish connection to database
+							connection = DriverManager.getConnection(DATABASE_URL, UserName_SQL, Password_SQL);
+								
+							// create Statement for querying database
+							statement = connection.createStatement();
+							
+							// Insert data into database
+							// statement.executeUpdate("INSERT INTO Product (productName, description, price)" + "VALUES" + "('" + productName + "','" + description + "','" +price)");
+							PreparedStatement pstat = connection.prepareStatement("INSERT INTO Product (productName, description, price) VALUES(?,?,?)");
+							pstat.setString(1, productName);
+							pstat.setString(2, description);
+							pstat.setString(3, price);
+							pstat.executeUpdate();
+								
+						}
+							
+						catch(SQLException sqlException) {
+							sqlException.printStackTrace();
+						}
+							
+						finally {
+							try {
+								statement.close();
+								connection.close();
+							}
+								
+							catch ( Exception exception ) {
+								exception.printStackTrace();
+							}
+						}
+						
+						JOptionPane.showMessageDialog(AddProductForm.this, String.format("Added to Database", event.getActionCommand()));
+						productNameField.setText("");
+						descriptionField.setText("");
+						priceField.setText("");
+					}
+					}
+					
+					
+				}
+				// clear button handler
+					private class ClearButtonHandler implements ActionListener {
+
+						@Override
+						public void actionPerformed(ActionEvent event) {
+							productNameField.setText("");
+							descriptionField.setText("");
+							priceField.setText("");
+						}
+						
+					}
+		} // end JFrame class
 	   
 	   	@Override
 		public void actionPerformed(ActionEvent e) {
